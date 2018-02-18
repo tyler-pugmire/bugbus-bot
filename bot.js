7e70dedd50b5f17b722ff5ea732eb98712007047
tyler-pugmire <tyler.pugmire@yahoo.com>
2018-02-18 12:16:11 -0800

    role checking works.
    Would like to improve how role checking is being handled.

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ function CustomCommand() {
 }
 
 var customCommand = [];
+var mod;
+var bestmod;
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -31,8 +33,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     var args = message.substring(1).split(' ');
     var cmd = args[0];
     var member = bot.servers[bot.channels[channelID].guild_id].members[userID];
-    var mod = bot.servers[bot.channels[channelID].guild_id].roles.find("name", "mod");
-    var bestmod = bot.servers[bot.channels[channelID].guild_id].roles.find("name", "!bestmod");
+    var roles = bot.servers[bot.channels[channelID].guild_id].roles;
+    for(var key in roles) {
+      if(roles[key].name == 'mod') {
+        mod = roles[key].id;
+      }
+      else if(roles[key].name == '!bestmod') {
+        bestmod = roles[key].id;
+      }
+    }
     
     args = args.splice(1);
     switch(cmd) {
@@ -42,7 +51,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       break;
       // Just add any case commands if you want to..
       case 'addcmd':
-        if(member.roles.has(mod.id) || member.roles.has(bestmod.id)){
+        if(member.roles.includes(mod) || member.roles.includes(bsetmod)) {
           var customcmd = new CustomCommand();
           customcmd.cmd = args[0];
           customcmd.msg = args[1]; 

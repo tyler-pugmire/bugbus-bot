8216fbd8de92d5d8c9dc17bab2eb0752efecfc8f
tyler-pugmire <tyler.pugmire@yahoo.com>
2018-02-18 11:51:51 -0800

    basic implementaion of discord bot.
    It doesnt currently work due to not checking roles correctly.

---
+++ bot.js
@@ -0,0 +1,61 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+function CustomCommand() {
+  this.cmd;
+  this.msg;
+}
+
+var customCommand = [];
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+  colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.substring(0, 1) == '!') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+    var member = bot.servers[bot.channels[channelID].guild_id].members[userID];
+    var mod = bot.servers[bot.channels[channelID].guild_id].roles.find("name", "mod");
+    var bestmod = bot.servers[bot.channels[channelID].guild_id].roles.find("name", "!bestmod");
+    
+    args = args.splice(1);
+    switch(cmd) {
+      // !ping
+      case 'ping':
+        bot.sendMessage({to: channelID, message: 'Pong!' });
+      break;
+      // Just add any case commands if you want to..
+      case 'addcmd':
+        if(member.roles.has(mod.id) || member.roles.has(bestmod.id)){
+          var customcmd = new CustomCommand();
+          customcmd.cmd = args[0];
+          customcmd.msg = args[1]; 
+          customCommand.push(customcmd);
+          bot.sendMessage({ to: channelID, message: args[0] });
+        }
+      break;
+    default:
+      for (var i = 0; i < customCommand.length; i++) {
+        if(cmd == customCommand[i].cmd) {
+          bot.sendMessage({ to: channelID, message: customCommand[i].msg });
+        }
+      }
+    }
+  }
+});

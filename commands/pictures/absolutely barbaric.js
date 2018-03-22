88af341a4518e08a28310d1a8a18e1b96e78ce5a
Aidan Chen <chen982003@gmail.com>
2018-03-21 19:11:30 -0700

    chase's request
    absolutely barbaric

---
+++ commands/pictures/absolutely barbaric.js
@@ -0,0 +1,26 @@
+﻿const commando = require('discord.js-commando');
+const fs = require('fs');
+const globals = JSON.parse(fs.readFileSync('./storage/globals.json', 'utf8'));
+
+class AbsolutelyBarbaricCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'absolutelybarbaric',
+      group: 'pictures',
+      memberName: 'absolutelybarbaric',
+      description: 'Absolutely Barbaric!'
+    });
+  }
+
+
+
+  async run(message, args) {
+    message.channel.send({embed: {
+      color : parseInt(globals.messageColor),
+      image: {
+        url: "https://cdn.discordapp.com/attachments/421143753319776276/425849577795747845/a14.png"
+      } 
+    }});
+  }
+}
+module.exports = AbsolutelyBarbaricCommand;

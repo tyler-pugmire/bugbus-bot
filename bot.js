d7d3f3f0ee4508dfaa3375ea99d00b1b7fa5ed31
tyler-pugmire <tyler.pugmire@yahoo.com>
2018-04-06 00:34:23 -0700

    Cleaned up the color command a lot.

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ const path = require('path');
 const sqlite = require('sqlite');
 const request = require('request');
 const oneLine = require('common-tags').oneLine;
+const fs = require('fs');
+const colors = JSON.parse(fs.readFileSync('./storage/colors.json', 'utf8')).Colors;
 
 
 //const func = require('./functions.js'); // If this returns an error for you (or you might be on ubuntu/linux), try '../functions.js'
@@ -18,7 +20,17 @@ bot.on('error', console.error)
   .on('warn', console.warn)
   .on('debug', console.log)
   .on('ready', () => {
-    console.log(`Bot ready; logged in as ${bot.user.username}#${bot.user.discriminator} (${bot.user.id})`);
+		console.log(`Bot ready; logged in as ${bot.user.username}#${bot.user.discriminator} (${bot.user.id})`);
+		let curColors = bot.guilds.get("414626170441564170").roles.filter(role => role.name.startsWith("$")).map(c => c.name.slice(1));
+		let colorDiff = colors.filter(x => !curColors.includes(x.name));
+		console.log(colorDiff);
+		for(var i = 0; i < colorDiff.length; ++i) {
+			bot.guilds.get("414626170441564170").createRole({
+				name: `$${colorDiff[i].name}`,
+				color: colorDiff[i].value,
+				permissions: []
+		 });
+		}
   })
   .on('disconnect', () => { console.warn('Disconnected!'); })
   .on('reconnecting', () => { console.warn('Reconnecting...'); })
@@ -51,6 +63,17 @@ bot.on('error', console.error)
 			${enabled ? 'enabled' : 'disabled'}
 			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
 		`);
+	})
+	.on('guildCreate', (guild) => {
+		let curColors = guild.roles.filter(role => role.name.startsWith("$")).map(c => c.name.slice(1));
+		let colorDiff = colors.filter(x => !curColors.includes(x.name));
+		for(var i = 0; i < colorDiff.length; ++i) {
+			guild.createRole({
+				name: `$${colorDiff[i].name}`,
+				color: colorDiff[i].value,
+				permissions: []
+		 });
+		}
 	});
 
 bot.setProvider(
@@ -64,7 +87,8 @@ bot.registry
 		['minecraft', 'Minecraft Commands'],
 		['yugioh', 'Yu-Gi-Oh Commands'],
 		['puyo-puyo', 'Puyo-Puyo commands'],
-		['stream', 'Stream Commands']
+		['stream', 'Stream Commands'],
+		['pokemon', 'Pokemon Commands']
 	])
 	.registerDefaultTypes()
   .registerCommandsIn(path.join(__dirname, 'commands'));
--- commands/util/color.js
+++ commands/util/color.js
@@ -21,90 +21,30 @@ class ColorCommand extends commando.Command {
   }
 
   async run(message, args) {
-    //if(args.length != 1) {
-    //  var colorString = "";
-    //  for(var key in colors) {
-    //    colorString += colors[key].name + ', ';
-    //  }
-    //  colorString = colorString.slice(0, -2);
-    //  message.channel.send({embed: {
-    //    color: parseInt(globals.errorColor),
-    //    description: `${message.author} Not correct number of args. Try one of these colors` + "```\n" + colorString + "\n```"
-    //  }})
-//
-    //  return;
-    //}
-    var guild = message.member.guild;
-    var validColor = false;
-    var name;
-    for(var key in colors) {
-      if(colors[key].name.toLowerCase() == args.colorName.toLowerCase()) {
-        name = key;
-        validColor = true;
-        break;
-      }
-    }
-
-    if(!validColor) {
-      var colorString = "";
-      for(var key in colors) {
-        colorString += colors[key].name + ', ';
-      }
-      colorString = colorString.slice(0, -2);
+    let colors = message.guild.roles.filter(role => role.name.startsWith("$"));
+    
+    let role = colors.find(role => role.name.slice(1).toLowerCase() === args.colorName.toLowerCase());
+    if(!role) {
       message.channel.send({embed: {
         color: parseInt(globals.errorColor),
-        description: `${message.author} Invalid color provided. Try one of these colors` + "```\n" + colorString + "\n```"
-      }})
-
+        description: `${message.author} Invalid color provided. Try one of these colors` + "```\n" + colors.map(x => " " + x.name.slice(1)) + "\n```"
+      }});
       return;
     }
 
-    const roleName = "Color_" + name;
-    /*Aqua, DarkAqua, Green, LightGreen, DarkGreen, 
-      Blue, DarkBlue, Cyan, DarkCyan, Purple, 
-      DarkPurple, Magenta, DarkMagenta, Gold, DarkGold, 
-      Orange, LightOrange, DarkOrange, Red, LightRed, 
-      DarkRed, Pink, Fuschia, Yellow, Black,
-      White, Navy, DarkNavy*/
-    const rolesArray = guild.roles.array();
-    var roleExists = false;
-    var setRole;
-    for(var i = 0; i < rolesArray.length; ++i) {
-      if(rolesArray[i].name === roleName) {
-        roleExists = true;
-        setRole = rolesArray[i];
-      }
-    }
-
-    var memberRoles = message.member.roles.array();
-    var removeRoles = [];
-    for(var i = 0; i < memberRoles.length; ++i) {
-      if(memberRoles[i].name.startsWith("Color_")) {
-        removeRoles.push(memberRoles[i]);
-      }
-    }
-    if(removeRoles.length != 0)
-      message.member.removeRoles(removeRoles);
-
-    if(roleExists) {
-      message.member.addRole(setRole);
+    try {
+      await message.member.removeRoles(colors);
+      await message.member.addRole(role);
       message.channel.send({embed: {
         color: parseInt(globals.successColor),
-        description: `${message.author} Color successfully changed to ${name}`
-      }})
-    }
-    else {
-      message.guild.createRole({
-         name: roleName,
-         color: colors[name].value,
-         permissions: []
-      }).then(function(role) {
-         message.member.addRole(role);
-         message.channel.send({embed: {
-          color: parseInt(globals.successColor),
-          description: `${message.author} Color successfully changed to ${name}`
-        }})
-      });
+        description: `${message.author} Color successfully changed to ${role.name}`
+      }});
+    } catch(e) {
+      message.channel.send({embed: {
+        color: parseInt(globals.errorColor),
+        description: `${message.author} something went wrong contact Tyler`
+      }});
+      console.log(e.message);
     }
   }
 }

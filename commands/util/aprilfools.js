503cdf778fb8bc2f67f26de8ddba9730c63f380d
Tyler Pugmire <tyler.pugmire@yahoo.com>
2020-03-31 18:42:04 -0700

    april fools 2020

---
+++ commands/util/aprilfools.js
@@ -0,0 +1,20 @@
+const commando = require('discord.js-commando');
+
+class AprilFools2020Command extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'aprilfools',
+      group: 'util',
+      memberName: 'aprilfools',
+      description: 'Toggle April Fools'
+    });
+  }
+
+  async run(message, args) {
+    require('./../../structures/april_fools_2020.js').enabled = !require('./../../structures/april_fools_2020.js').enabled;
+    if(enabled == true) {
+        
+    }
+  }
+}
+module.exports = AprilFools2020Command;
